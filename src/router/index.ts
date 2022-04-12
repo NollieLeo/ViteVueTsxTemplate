d15cbe22417eccaf5b4c180f6d5ece3b7b723ffe
翁恺敏 <wengkaimin@kanzhun.com>
2022-04-12 16:38:16 +0800

    feat: add redme

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@
  * @Author: 翁恺敏
  * @Date: 2022-04-12 15:48:26
  * @LastEditors: 翁恺敏
- * @LastEditTime: 2022-04-12 15:48:27
+ * @LastEditTime: 2022-04-12 16:34:28
  * @FilePath: /vue3-vite-test/src/router/index.ts
  * @Description:
  */
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
